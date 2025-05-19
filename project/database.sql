64400f6165c06ed32d9727287935b3e9adafb798
Hannah Law <hannah.lch.law@gmail.com>
2025-05-19 16:52:25 +1000

    Update database.sql
    Update and Insert database.sql

--- project/database.sql
+++ project/database.sql
@@ -71,7 +71,7 @@ FOREIGN KEY (itemCode) REFERENCES items (itemCode)
 );
 
 CREATE TABLE delivery_methods (
-deliveryMethodID INT AUTO_INCREMENT PRIMARY KEY,
+deliveryMethodCode ENUM('STANDARD', 'ECO', 'EXPRESS', 'TEMP') PRIMARY KEY,
 deliveryMethodName VARCHAR (50) NOT NULL,
 surchargePrice DECIMAL(10,2) NOT NULL
 );
@@ -83,11 +83,11 @@ basketID INT NOT NULL,
 customerID INT NOT NULL,
 orderDate DATETIME DEFAULT CURRENT_TIMESTAMP,
 order_status ENUM('Pending', 'Confirmed', 'Cancelled') DEFAULT 'Pending',
-deliveryMethodID INT NOT NULL,
+deliveryMethodCode ENUM('STANDARD', 'ECO', 'EXPRESS', 'TEMP') NOT NULL,
 updatedDate DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 FOREIGN KEY (basketID) REFERENCES baskets (basketID) ON DELETE CASCADE,
 FOREIGN KEY (customerID) REFERENCES customers (customerID) ON DELETE CASCADE,
-FOREIGN KEY (deliveryMethodID) REFERENCES delivery_methods (deliveryMethodID)
+FOREIGN KEY (deliveryMethodCode) REFERENCES delivery_methods (deliveryMethodCode)
 );
 
 CREATE TABLE payments (
@@ -103,3 +103,78 @@ updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 FOREIGN KEY (orderID) REFERENCES orders (orderID) ON DELETE CASCADE,
 FOREIGN KEY (customerID) REFERENCES customers (customerID) ON DELETE CASCADE
 );
+
+-- Customers
+INSERT INTO customers VALUES (NULL, 'Hannah', 'Law', '0432274880', 'N12229628@qut.edu.au', '2 George Street', 'Gardens Point', 'Brisbane', 'Queensland', '4000');
+INSERT INTO customers VALUES (NULL, 'Luke', 'Benjapattranon', NULL, 'N12258164@qut.edu.au', '2 George Street', 'Gardens Point', 'Brisbane', 'Queensland', '4000');
+INSERT INTO customers VALUES (NULL, 'Elsie', 'Shim', NULL, 'N12219151@qut.edu.au', '2 George Street', 'Gardens Point', 'Brisbane', 'Queensland', '4000');
+INSERT INTO customers VALUES (NULL, 'Monica', 'Nunes', NULL, 'N12240672@qut.edu.au', '2 George Street', 'Gardens Point', 'Brisbane', 'Queensland', '4000');
+
+-- Users
+REPLACE INTO users VALUES (NULL, 'SYSADMIN', NULL, '0000000000', 'Admin', DEFAULT, DEFAULT);
+INSERT INTO users VALUES (NULL, 'NABILA', NULL, '0000000000', 'Admin', DEFAULT, DEFAULT);
+INSERT INTO users VALUES (NULL, 'HANNAH', '1', '0000000000', 'User', DEFAULT, DEFAULT);
+INSERT INTO users VALUES (NULL, 'LUKE', '2', '0000000000', 'User', DEFAULT, DEFAULT);
+INSERT INTO users VALUES (NULL, 'ELSIE','3', '0000000000', 'User', DEFAULT, DEFAULT);
+INSERT INTO users VALUES (NULL, 'MONICA', '4', '0000000000', 'User', DEFAULT, DEFAULT);
+
+-- suppliers
+INSERT INTO suppliers VALUES (NULL, 'Gardens Point Medical Inc.');
+INSERT INTO suppliers VALUES (NULL, 'Kelvin Grove Medical Ltd.');
+
+-- category
+INSERT INTO category VALUES ('MD', 'Medicine');
+INSERT INTO category VALUES ('MU', 'Makeup');
+INSERT INTO category VALUES ('SC', 'Skincare');
+
+-- items
+INSERT INTO items VALUES ('38762059', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 10 pack', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 10 pack', NULL, NULL, '13.99', '9.50', '1', 'MD', '100');
+INSERT INTO items VALUES ('13982406', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 80 pack', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 80 pack', NULL, NULL, '33.99', NULL, '1', 'MD', '100');
+INSERT INTO items VALUES ('72145608', 'CeraVe Skin Renewing Night Cream 48g', 'CeraVe Skin Renewing Night Cream 48g', NULL, NULL, '41.50', '28.69', '2', 'SC', '100');
+INSERT INTO items VALUES ('21458930', 'CeraVe Skin Renewing Night Cream 100g', 'CeraVe Skin Renewing Night Cream 100g', NULL, NULL, '79.99', NULL, '2', 'SC', '100');
+INSERT INTO items VALUES ('26873194', 'CeraVe Moisturising Cream 170g', 'CeraVe Moisturising Cream 170g', NULL, NULL, '16.00', '11.19', '2', 'SC', '100');
+INSERT INTO items VALUES ('36790128', 'CeraVe Moisturising Cream 454g', 'CeraVe Moisturising Cream 454g', NULL, NULL, '25.99', NULL, '2', 'SC', '100');
+INSERT INTO items VALUES ('94018536', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Mint', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Mint', NULL, NULL, '11.99', '10.99', '1', 'MD', '100');
+INSERT INTO items VALUES ('90871245', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Watermelon', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Watermelon', NULL, NULL, '11.99', '10.99', '1', 'MD', '100');
+INSERT INTO items VALUES ('62846620', 'Maybelline Fit Me True-to-tone Blush - Rose', 'Maybelline Fit Me True-to-tone Blush - Rose', NULL, NULL, '9.49', '8.99', '2', 'MU', '100');
+INSERT INTO items VALUES ('85623014', 'Maybelline Fit Me True-to-tone Blush - Coral', 'Maybelline Fit Me True-to-tone Blush - Coral', NULL, NULL, '9.49', '8.99', '2', 'MU', '100');
+INSERT INTO items VALUES ('80439217', 'Maybelline Lasting Fix Setting Loose Powder', 'Maybelline Lasting Fix Setting Loose Powder', NULL, NULL, '9.99', '9.49', '2', 'MU', '100');
+INSERT INTO items VALUES ('60381297', 'Maybelline Lasting Fix Loose Translucent Setting Powder', 'Maybelline Lasting Fix Loose Translucent Setting Powder', NULL, NULL, '9.99', NULL, '2', 'MU', '100');
+INSERT INTO items VALUES ('17530942', 'Nurofen Zavance 96 Tablets', 'Nurofen Zavance 96 Tablets', NULL, NULL, '28.99', '20.99', '1', 'MD', '100');
+INSERT INTO items VALUES ('74023185', 'Nurofen Zavance 96 Caplets', 'Nurofen Zavance 96 Caplets', NULL, NULL, '28.99', '20.99', '1', 'MD', '100');
+INSERT INTO items VALUES ('58219437', 'Panadol Rapid 48 Caplets', 'Panadol Rapid 48 Caplets', NULL, NULL, '11.99', NULL, '1', 'MD', '100');
+INSERT INTO items VALUES ('49217683', 'Panadol Rapid 48 Tablets', 'Panadol Rapid 48 Tablets', NULL, NULL, '8.99', NULL, '1', 'MD', '100');
+
+-- delivery_methods
+INSERT INTO delivery_methods VALUES ('STANDARD', 'Standard Delivery', '5.00');
+INSERT INTO delivery_methods VALUES ('ECO', 'Eco-Friendly Delivery', '8.00');
+INSERT INTO delivery_methods VALUES ('EXPRESS', 'Express Delivery', '15.00');
+INSERT INTO delivery_methods VALUES ('TEMP', 'Temperature-Controlled Delivery', '25.00');
+
+-- baskets
+INSERT INTO baskets VALUES (NULL, '2');
+INSERT INTO baskets VALUES (NULL, '4');
+INSERT INTO baskets VALUES (NULL, '1');
+INSERT INTO baskets VALUES (NULL, '3');
+
+-- basket_items
+INSERT INTO basket_items VALUES (NULL, 1, '13982406', DEFAULT);
+INSERT INTO basket_items VALUES (NULL, 1, '94018536', DEFAULT);
+INSERT INTO basket_items VALUES (NULL, 2, '80439217', DEFAULT);
+INSERT INTO basket_items VALUES (NULL, 2, '17530942', DEFAULT);
+INSERT INTO basket_items VALUES (NULL, 2, '58219437', DEFAULT);
+INSERT INTO basket_items VALUES (NULL, 3, '74023185', DEFAULT);
+INSERT INTO basket_items VALUES (NULL, 3, '60381297', DEFAULT);
+INSERT INTO basket_items VALUES (NULL, 3, '49217683', DEFAULT);
+
+-- orders
+INSERT INTO orders VALUES (NULL, '1001', '1', '1', DEFAULT, DEFAULT, 'STANDARD', DEFAULT);
+INSERT INTO orders VALUES (NULL, '1002', '2', '3', DEFAULT, DEFAULT, 'ECO', DEFAULT);
+INSERT INTO orders VALUES (NULL, '1003', '3', '4', DEFAULT, DEFAULT, 'EXPRESS', DEFAULT);
+INSERT INTO orders VALUES (NULL, '1004', '4', '2', DEFAULT, DEFAULT, 'TEMP', DEFAULT);
+
+-- payments
+INSERT INTO payments VALUES (NULL, '91001', 'Credit Card', 'Hannah Law', DEFAULT, '1', '1', DEFAULT, DEFAULT);
+INSERT INTO payments VALUES (NULL, '91002', 'Debit Card', 'Monica Nunes', DEFAULT, '2', '4', DEFAULT, DEFAULT);
+INSERT INTO payments VALUES (NULL, '91003', 'After Pay', 'Elsie Shim', DEFAULT, '3', '3', DEFAULT, DEFAULT);
+INSERT INTO payments VALUES (NULL, '91003', 'After Pay', 'Luke Benjapattranon', DEFAULT, '4', '2', DEFAULT, DEFAULT);
