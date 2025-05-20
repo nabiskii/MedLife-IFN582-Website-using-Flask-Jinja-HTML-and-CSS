1789b92404981f9613d72cc78ef812846af10adf
Hannah Law <hannah.lch.law@gmail.com>
2025-05-20 14:05:50 +1000

    Update database.sql

--- project/database.sql
+++ project/database.sql
@@ -60,6 +60,9 @@ FOREIGN KEY (supplierID) REFERENCES suppliers (supplierID)
 CREATE TABLE baskets (
 basketID INT AUTO_INCREMENT PRIMARY KEY,
 customerID INT NOT NULL,
+promotionCode VARCHAR(15),
+discountAmount DECIMAL(10,2),
+CHECK (promotionCode IS NULL OR discountAmount IS NOT NULL),
 FOREIGN KEY (customerID) REFERENCES customers (customerID) ON DELETE CASCADE
 );
 
@@ -85,7 +88,7 @@ orderNumber INT NOT NULL,
 basketID INT NOT NULL,
 customerID INT NOT NULL,
 orderDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-order_status ENUM('Pending', 'Confirmed', 'Cancelled') DEFAULT 'Pending',
+orderStatus ENUM('Pending', 'Confirmed', 'Cancelled') DEFAULT 'Pending',
 deliveryMethodCode ENUM('STANDARD', 'ECO', 'EXPRESS', 'TEMP') NOT NULL,
 updatedDate DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 FOREIGN KEY (basketID) REFERENCES baskets (basketID) ON DELETE CASCADE,
@@ -101,6 +104,7 @@ payeeName VARCHAR(50) NOT NULL,
 paymentDate DATETIME DEFAULT CURRENT_TIMESTAMP,
 orderID INT NOT NULL,
 customerID INT NOT NULL,
+paymentStatus ENUM('Pending', 'Confirmed', 'Cancelled') DEFAULT 'Pending',
 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
 updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 FOREIGN KEY (orderID) REFERENCES orders (orderID) ON DELETE CASCADE,
@@ -132,22 +136,22 @@ INSERT INTO category VALUES ('MU', 'Makeup');
 INSERT INTO category VALUES ('SC', 'Skincare');
 
 -- items
-INSERT INTO items VALUES ('38762059', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 10 pack', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 10 pack', NULL, NULL, '13.99', '9.50', '1', 'MD', '100');
-INSERT INTO items VALUES ('13982406', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 80 pack', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 80 pack', NULL, NULL, '33.99', NULL, '1', 'MD', '100');
-INSERT INTO items VALUES ('72145608', 'CeraVe Skin Renewing Night Cream 48g', 'CeraVe Skin Renewing Night Cream 48g', NULL, NULL, '41.50', '28.69', '2', 'SC', '100');
-INSERT INTO items VALUES ('21458930', 'CeraVe Skin Renewing Night Cream 100g', 'CeraVe Skin Renewing Night Cream 100g', NULL, NULL, '79.99', NULL, '2', 'SC', '100');
-INSERT INTO items VALUES ('26873194', 'CeraVe Moisturising Cream 170g', 'CeraVe Moisturising Cream 170g', NULL, NULL, '16.00', '11.19', '2', 'SC', '100');
-INSERT INTO items VALUES ('36790128', 'CeraVe Moisturising Cream 454g', 'CeraVe Moisturising Cream 454g', NULL, NULL, '25.99', NULL, '2', 'SC', '100');
-INSERT INTO items VALUES ('94018536', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Mint', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Mint', NULL, NULL, '11.99', '10.99', '1', 'MD', '100');
-INSERT INTO items VALUES ('90871245', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Watermelon', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Watermelon', NULL, NULL, '11.99', '10.99', '1', 'MD', '100');
-INSERT INTO items VALUES ('62846620', 'Maybelline Fit Me True-to-tone Blush - Rose', 'Maybelline Fit Me True-to-tone Blush - Rose', NULL, NULL, '9.49', '8.99', '2', 'MU', '100');
-INSERT INTO items VALUES ('85623014', 'Maybelline Fit Me True-to-tone Blush - Coral', 'Maybelline Fit Me True-to-tone Blush - Coral', NULL, NULL, '9.49', '8.99', '2', 'MU', '100');
-INSERT INTO items VALUES ('80439217', 'Maybelline Lasting Fix Setting Loose Powder', 'Maybelline Lasting Fix Setting Loose Powder', NULL, NULL, '9.99', '9.49', '2', 'MU', '100');
-INSERT INTO items VALUES ('60381297', 'Maybelline Lasting Fix Loose Translucent Setting Powder', 'Maybelline Lasting Fix Loose Translucent Setting Powder', NULL, NULL, '9.99', NULL, '2', 'MU', '100');
-INSERT INTO items VALUES ('17530942', 'Nurofen Zavance 96 Tablets', 'Nurofen Zavance 96 Tablets', NULL, NULL, '28.99', '20.99', '1', 'MD', '100');
-INSERT INTO items VALUES ('74023185', 'Nurofen Zavance 96 Caplets', 'Nurofen Zavance 96 Caplets', NULL, NULL, '28.99', '20.99', '1', 'MD', '100');
-INSERT INTO items VALUES ('58219437', 'Panadol Rapid 48 Caplets', 'Panadol Rapid 48 Caplets', NULL, NULL, '11.99', NULL, '1', 'MD', '100');
-INSERT INTO items VALUES ('49217683', 'Panadol Rapid 48 Tablets', 'Panadol Rapid 48 Tablets', NULL, NULL, '8.99', NULL, '1', 'MD', '100');
+INSERT INTO items VALUES ('38762059', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 10 pack', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 10 pack', NULL, NULL, '13.99', '9.50', '1', 'MD', '100', NULL);
+INSERT INTO items VALUES ('13982406', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 80 pack', 'Claratyne Allergy & Hayfever Relief Antihistamine Tablets 80 pack', NULL, NULL, '33.99', NULL, '1', 'MD', '100', NULL);
+INSERT INTO items VALUES ('72145608', 'CeraVe Skin Renewing Night Cream 48g', 'CeraVe Skin Renewing Night Cream 48g', NULL, NULL, '41.50', '28.69', '2', 'SC', '100', NULL);
+INSERT INTO items VALUES ('21458930', 'CeraVe Skin Renewing Night Cream 100g', 'CeraVe Skin Renewing Night Cream 100g', NULL, NULL, '79.99', NULL, '2', 'SC', '100', NULL);
+INSERT INTO items VALUES ('26873194', 'CeraVe Moisturising Cream 170g', 'CeraVe Moisturising Cream 170g', NULL, NULL, '16.00', '11.19', '2', 'SC', '100', NULL);
+INSERT INTO items VALUES ('36790128', 'CeraVe Moisturising Cream 454g', 'CeraVe Moisturising Cream 454g', NULL, NULL, '25.99', NULL, '2', 'SC', '100', NULL);
+INSERT INTO items VALUES ('94018536', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Mint', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Mint', NULL, NULL, '11.99', '10.99', '1', 'MD', '100', NULL);
+INSERT INTO items VALUES ('90871245', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Watermelon', 'Difflam Plus Sore Throat Anaesthetic Spray 30ml - Watermelon', NULL, NULL, '11.99', '10.99', '1', 'MD', '100', NULL);
+INSERT INTO items VALUES ('62846620', 'Maybelline Fit Me True-to-tone Blush - Rose', 'Maybelline Fit Me True-to-tone Blush - Rose', NULL, NULL, '9.49', '8.99', '2', 'MU', '100', NULL);
+INSERT INTO items VALUES ('85623014', 'Maybelline Fit Me True-to-tone Blush - Coral', 'Maybelline Fit Me True-to-tone Blush - Coral', NULL, NULL, '9.49', '8.99', '2', 'MU', '100', NULL);
+INSERT INTO items VALUES ('80439217', 'Maybelline Lasting Fix Setting Loose Powder', 'Maybelline Lasting Fix Setting Loose Powder', NULL, NULL, '9.99', '9.49', '2', 'MU', '100', NULL);
+INSERT INTO items VALUES ('60381297', 'Maybelline Lasting Fix Loose Translucent Setting Powder', 'Maybelline Lasting Fix Loose Translucent Setting Powder', NULL, NULL, '9.99', NULL, '2', 'MU', '100', NULL);
+INSERT INTO items VALUES ('17530942', 'Nurofen Zavance 96 Tablets', 'Nurofen Zavance 96 Tablets', NULL, NULL, '28.99', '20.99', '1', 'MD', '100', NULL);
+INSERT INTO items VALUES ('74023185', 'Nurofen Zavance 96 Caplets', 'Nurofen Zavance 96 Caplets', NULL, NULL, '28.99', '20.99', '1', 'MD', '100', NULL);
+INSERT INTO items VALUES ('58219437', 'Panadol Rapid 48 Caplets', 'Panadol Rapid 48 Caplets', NULL, NULL, '11.99', NULL, '1', 'MD', '100', NULL);
+INSERT INTO items VALUES ('49217683', 'Panadol Rapid 48 Tablets', 'Panadol Rapid 48 Tablets', NULL, NULL, '8.99', NULL, '1', 'MD', '100', NULL);
 
 -- delivery_methods
 INSERT INTO delivery_methods VALUES ('STANDARD', 'Standard Delivery', '5.00');
@@ -156,10 +160,10 @@ INSERT INTO delivery_methods VALUES ('EXPRESS', 'Express Delivery', '15.00');
 INSERT INTO delivery_methods VALUES ('TEMP', 'Temperature-Controlled Delivery', '25.00');
 
 -- baskets
-INSERT INTO baskets VALUES (NULL, '2');
-INSERT INTO baskets VALUES (NULL, '4');
-INSERT INTO baskets VALUES (NULL, '1');
-INSERT INTO baskets VALUES (NULL, '3');
+INSERT INTO baskets VALUES (NULL, '2', NULL, NULL);
+INSERT INTO baskets VALUES (NULL, '4', 'SAVE5NOWMAY', '5.00');
+INSERT INTO baskets VALUES (NULL, '1', 'SAVE5NOWMAY', '5.00');
+INSERT INTO baskets VALUES (NULL, '3', NULL, NULL);
 
 -- basket_items
 INSERT INTO basket_items VALUES (NULL, 1, '13982406', DEFAULT);
@@ -179,15 +183,15 @@ INSERT INTO orders VALUES (3, '1003', '4', '3', DEFAULT, DEFAULT, 'EXPRESS', DEF
 INSERT INTO orders VALUES (4, '1004', '2', '4', DEFAULT, DEFAULT, 'TEMP', DEFAULT);
 
 -- payments
-INSERT INTO payments VALUES (NULL, '91001', 'Credit Card', 'Hannah Law', DEFAULT, '1', '1', DEFAULT, DEFAULT);
-INSERT INTO payments VALUES (NULL, '91002', 'Debit Card', 'Luke Benjapattranon', DEFAULT, '2', '2', DEFAULT, DEFAULT);
-INSERT INTO payments VALUES (NULL, '91003', 'After Pay', 'Elsie Shim', DEFAULT, '3', '3', DEFAULT, DEFAULT);
-INSERT INTO payments VALUES (NULL, '91003', 'After Pay', 'Monica Nunes', DEFAULT, '4', '4', DEFAULT, DEFAULT);
+INSERT INTO payments VALUES (NULL, '91001', 'Credit Card', 'Hannah Law', DEFAULT, '1', '1', DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO payments VALUES (NULL, '91002', 'Debit Card', 'Luke Benjapattranon', DEFAULT, '2', '2', DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO payments VALUES (NULL, '91003', 'After Pay', 'Elsie Shim', DEFAULT, '3', '3', DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO payments VALUES (NULL, '91004', 'After Pay', 'Monica Nunes', DEFAULT, '4', '4', DEFAULT, DEFAULT, DEFAULT);
 
 ------------------------------------------------------------------------------------------------------------------------------------------
--- customers view table
-CREATE VIEW customers_v AS 
+-- customers read table
 SELECT
+	customerID AS 'Customer ID',
 	CONCAT(firstName, ' ', surname) AS 'Customer Name',
     phoneNumber AS 'Phone Number',
 	emailAddress AS 'Email Address',
@@ -196,21 +200,26 @@ SELECT
 	city AS 'City',
 	state AS 'State',
 	postCode AS 'Post Code'
-FROM customers;
+FROM customers
+ORDER BY
+	customerID;
 
--- suppliers view table
-CREATE VIEW suppliers_v AS 
+-- suppliers read table
 SELECT
 	supplierName AS 'Supplier Name'
-FROM suppliers;
+FROM suppliers
+ORDER BY
+	supplierName;
 
-CREATE VIEW category_v AS 
+-- category read table
 SELECT
+	categoryCode AS 'Category Code',
 	categoryName AS 'Category Name'
-FROM category;
+FROM category
+ORDER BY
+	categoryCode;
 
--- users view table
-CREATE VIEW users_v AS 
+-- users read table
 SELECT
 	users.userID AS 'User ID',
 	users.userName AS 'User Name',
@@ -220,10 +229,11 @@ SELECT
 	users.createdAt AS 'Creation Date & Time',
 	users.updatedAt AS 'Updated Date & Time'
 FROM users
-LEFT JOIN customers ON customers.customerID = users.customerID;
+LEFT JOIN customers ON customers.customerID = users.customerID
+ORDER BY
+	users.userID;
 
--- items view table
-CREATE VIEW items_v AS 
+-- items read table
 SELECT
 	items.itemCode AS 'Item Code',
 	items.itemName AS 'Item Name',
@@ -239,42 +249,49 @@ SELECT
 FROM items
 LEFT JOIN 
 	(category, suppliers) ON (category.categoryCode = items.categoryCode
-    AND suppliers.supplierID = items.supplierID);
+    AND suppliers.supplierID = items.supplierID)
+ORDER BY
+	items.itemName;
 	
--- baskets view table
-CREATE VIEW baskets_v AS 
+-- baskets read table
 SELECT
+	baskets.basketID AS 'Basket ID',
     CONCAT(customers.firstName, ' ', customers.surname) AS 'Customer Name',
     basket_items.itemCode AS 'Item Code',
     items.itemName AS 'Item Name',
     SUM(basket_items.quantity) AS 'Quantity',
 	IFNULL(items.discountPrice, items.unitPrice) AS 'Price',
-    SUM(basket_items.quantity * IFNULL(items.discountPrice, items.unitPrice)) AS 'Total Price'
+    SUM(basket_items.quantity * IFNULL(items.discountPrice, items.unitPrice)) AS 'Total Amount'
 FROM basket_items
 JOIN
 	(baskets, customers, items) ON (baskets.basketID = basket_items.basketID
 	AND customers.customerID = baskets.customerID
 	AND items.itemCode = basket_items.itemCode)
 GROUP BY
+	baskets.basketID,
     customers.firstName,
 	customers.surname,
     basket_items.itemCode,
     items.itemName,
-    items.discountPrice;
+    items.discountPrice
+ORDER BY 
+	baskets.basketID;
 	
--- orders view table
-CREATE VIEW orders_v AS 
+-- orders read table
 SELECT
 	orders.orderID AS 'Order ID',
 	orders.orderNumber AS 'Order Number',
 	orders.orderDate AS 'Order Date & Time',
-	orders.order_status AS 'Order Status',
+	orders.orderStatus AS 'Order Status',
     CONCAT(customers.firstName, ' ', customers.surname) AS 'Customer Name',
-    delivery_methods.deliveryMethodName AS 'Delivery Method',
-    delivery_methods.surchargePrice AS 'Delivery Fee',
     COUNT(DISTINCT basket_items.itemCode) AS 'Item Count',
     SUM(basket_items.quantity) AS 'Order Quantity',
-    SUM(basket_items.quantity * IFNULL(items.discountPrice, items.unitPrice)) AS 'Order Price',
+    SUM(basket_items.quantity * IFNULL(items.discountPrice, items.unitPrice)) AS 'Order Amount',
+	baskets.promotionCode AS 'Promotion Code',
+	baskets.discountAmount AS 'Discount Amount',
+    delivery_methods.deliveryMethodName AS 'Delivery Method',
+    delivery_methods.surchargePrice AS 'Delivery Fee',
+	SUM(basket_items.quantity * IFNULL(items.discountPrice, items.unitPrice)) + delivery_methods.surchargePrice - IFNULL(baskets.discountAmount, 0) AS 'Order Total Amount',
 	orders.updatedDate AS 'Updated Date & Time'
 FROM orders
 LEFT JOIN 
@@ -287,40 +304,47 @@ GROUP BY
 	orders.orderID,
     orders.orderNumber,
     orders.orderDate,
-    orders.order_status,
+    orders.orderStatus,
     customers.firstName,
     customers.surname,
     delivery_methods.deliveryMethodName,
     delivery_methods.surchargePrice,
-    orders.updatedDate;
+    orders.updatedDate
+ORDER BY
+	orders.orderID;
 	
--- payments view table
-CREATE OR REPLACE VIEW payments_v AS 
+-- payments read table
 SELECT
 	payments.paymentNumber AS 'Payment Number',
 	payments.paymentMethod AS 'Payment Method',
 	payments.payeeName AS 'Payee Name',
 	payments.paymentDate AS 'Payment Date & Time',
 	orders.orderNumber AS 'Order Number',
-	(SUM(basket_items.quantity * IFNULL(items.discountPrice, items.unitPrice)) + delivery_methods.surchargePrice) AS 'Payment Amount',
+	SUM(basket_items.quantity * IFNULL(items.discountPrice, items.unitPrice)) + delivery_methods.surchargePrice - IFNULL(baskets.discountAmount, 0) AS 'Payment Amount',
     CONCAT(customers.firstName, ' ', customers.surname) AS 'Customer Name',
+	payments.paymentStatus AS 'Payment Status',
 	payments.createdAt AS 'Creation Date & Time',
 	payments.updatedAt AS 'Updated Date & Time'
 FROM payments
 JOIN 
-	(orders, customers, basket_items, items, delivery_methods) ON (orders.orderID = payments.orderID
+	(orders, customers, baskets, basket_items, items, delivery_methods) ON (orders.orderID = payments.orderID
 	AND customers.customerID = payments.customerID
+	AND baskets.customerID = payments.customerID
     AND basket_items.basketID = orders.basketID
     AND items.itemCode = basket_items.itemCode
     AND delivery_methods.deliveryMethodCode = orders.deliveryMethodCode)
 GROUP BY
-	payments.paymentNumber,
-	payments.paymentMethod,
-	payments.payeeName,
-	payments.paymentDate,
-	orders.orderNumber,
-	delivery_methods.surchargePrice,
-	customers.firstName,
-	customers.surname,
-	payments.createdAt,
-	payments.updatedAt;
+    payments.paymentNumber,
+    payments.paymentMethod,
+    payments.payeeName,
+    payments.paymentDate,
+    orders.orderNumber,
+    delivery_methods.surchargePrice,
+    baskets.discountAmount,
+    customers.firstName,
+    customers.surname,
+    payments.paymentStatus,
+    payments.createdAt,
+    payments.updatedAt
+ORDER BY
+	payments.paymentNumber;
